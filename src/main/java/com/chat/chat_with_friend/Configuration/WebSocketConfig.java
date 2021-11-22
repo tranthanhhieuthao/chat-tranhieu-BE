207fc81af453645cd66e3cc0c4ef613cce646a8e
tranthanhhieuthao <tranthanhhieuthao@gmail.com>
2021-11-22 22:30:38 +0700

    make it better

--- src/main/java/com/chat/chat_with_friend/Configuration/WebSocketConfig.java
+++ src/main/java/com/chat/chat_with_friend/Configuration/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.chat.chat_with_friend.Configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -11,10 +12,13 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    @Autowired
+    private Environment evn;
+
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("http://localhost:8080/").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOriginPatterns(evn.getProperty("cors.url.allowed")).withSockJS();
     }
 
     @Override
--- src/main/java/com/chat/chat_with_friend/Configuration/Webconfig.java
+++ src/main/java/com/chat/chat_with_friend/Configuration/Webconfig.java
@@ -1,6 +1,8 @@
 package com.chat.chat_with_friend.Configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,10 +11,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class Webconfig implements WebMvcConfigurer {
 
+    @Autowired
+    private Environment evn;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:8080/")
+                .allowedOrigins(evn.getProperty("cors.url.allowed"))
                 .allowedMethods("GET", "POST")
                 .allowCredentials(true).maxAge(3600);
     }
